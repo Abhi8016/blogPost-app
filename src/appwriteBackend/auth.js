5bba339a7e6e4877f92e6f6f99788b9b0ad14f0c
Abhinaba <abhi.roychoudhury.8@gmail.com>
2023-12-12 05:17:26 +0530

    config file and appwrite auth.js done

---
+++ src/appwriteBackend/auth.js
@@ -0,0 +1,54 @@
+import conf from "../config/conf";
+import { Account, Client, ID } from "appwrite";
+
+export class AuthSrevice {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(ID.unique(),email,password,name);
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+      // await this.account.deleteSession("current")
+    } catch (error) {
+      console.log("Appwrite serive :: logout :: error", error);
+    }
+  }
+}
+
+const authService = new AuthSrevice();
+
+export default authService;
